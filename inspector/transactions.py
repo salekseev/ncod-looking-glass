44d6606a67887dd17c5be76ef83fb97a6699a667
Stas Alekseev <stas.alekseev@gmail.com>
2018-11-16 16:55:49 -0500

    updates to output of transactions

--- inspector/transactions.py
+++ inspector/transactions.py
@@ -19,13 +19,23 @@ def get(event, context):
         # Expression Attribute Names for Projection Expression only.
         ExpressionAttributeNames={"#timestamp": "timestamp"},
         KeyConditionExpression=Key('sysid').eq(event['pathParameters']['id']),
-        ScanIndexForward=True
+        ScanIndexForward=False
     )
+
+    new_items = []
+
+    for item in result['Items']:
+        new_item = {
+            'timestamp': datetime.datetime.utcfromtimestamp(item['timestamp']).strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'subsystem': item['subsystem'],
+            'payload': item['payload']
+        }
+        new_items.append(new_item)
 
     # create a response
     response = {
         "statusCode": 200,
-        "body": json.dumps(result['Items'], cls=decimalencoder.DecimalEncoder, indent=4, sort_keys=True)
+        "body": json.dumps(new_items, indent=2)
     }
 
     return response
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import boto3
 from inspector import decimalencoder
 from boto3.dynamodb.conditions import Key, Attr
 
-now = datetime.datetime.now()
+now = datetime.datetime.utcnow()
 dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
 # To go from low-level format to python
 deserializer = boto3.dynamodb.types.TypeDeserializer()
@@ -22,13 +22,23 @@ def main():
         ExpressionAttributeNames={"#timestamp": "timestamp"},
         KeyConditionExpression=Key('sysid').eq(
             'b3524e157e7d7bbca1859c4aeb7729cd'),
-        ScanIndexForward=True
+        ScanIndexForward=False
     )
+
+    new_items = []
+
+    for item in result['Items']:
+        new_item = {
+            'timestamp': datetime.datetime.utcfromtimestamp(item['timestamp']).strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'subsystem': item['subsystem'],
+            'payload': item['payload']
+        }
+        new_items.append(new_item)
 
     # create a response
     response = {
         "statusCode": 200,
-        "body": json.dumps(result['Items'], cls=decimalencoder.DecimalEncoder)
+        "body": json.dumps(new_items, cls=decimalencoder.DecimalEncoder)
     }
 
     return response
